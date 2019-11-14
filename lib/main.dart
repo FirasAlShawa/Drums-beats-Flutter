73b0a34dcca81e35f9e28fd9146ad9fe4e9853ef
firasShawa <firas.shawa22@gmail.com>
2019-11-14 12:09:13 +0300

    Home Widgit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Container());
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+      ),
+    );
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("Drum Beats"),
+      ),
+    );
+  }
+}
