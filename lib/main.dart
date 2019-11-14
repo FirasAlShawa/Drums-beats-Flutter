ceb32859001a7c6fd73eb0902d17d3148f1398ab
firasShawa <firas.shawa22@gmail.com>
2019-11-14 12:35:42 +0300

    Structuring Drumbody widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -15,6 +16,75 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.green,
         title: Text("Drum Beats"),
       ),
+      body: DrumBody(),
+    );
+  }
+}
+
+class DrumBody extends StatefulWidget {
+  @override
+  _DrumBodyState createState() => _DrumBodyState();
+}
+
+class _DrumBodyState extends State<DrumBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            FlatButton(
+              color: Colors.white,
+              child: Text("Beat 1"),
+              onPressed: () {},
+            ),
+            FlatButton(
+              color: Colors.white,
+              child: Text("Beat 2"),
+              onPressed: () {},
+            ),
+            FlatButton(
+              color: Colors.white,
+              child: Text("Beat 3"),
+              onPressed: () {},
+            ),
+            FlatButton(
+              color: Colors.white,
+              child: Text("Beat 4"),
+              onPressed: () {},
+            )
+          ],
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            FlatButton(
+              color: Colors.white,
+              child: Text("Beat 1"),
+              onPressed: () {},
+            ),
+            FlatButton(
+              color: Colors.white,
+              child: Text("Beat 2"),
+              onPressed: () {},
+            ),
+            FlatButton(
+              color: Colors.white,
+              child: Text("Beat 3"),
+              onPressed: () {},
+            ),
+            FlatButton(
+              color: Colors.white,
+              child: Text("Beat 4"),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ],
     );
   }
 }
